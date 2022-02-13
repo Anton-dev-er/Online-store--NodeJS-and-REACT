eb4b9cdff77485809044033a33628af5540ba74f
Anton2156 <akkforwork11@gmail.com>
2022-02-13 15:47:14 +0200

    [auth]

--- server/routes/genderRouter.js
+++ server/routes/genderRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const genderController = require('../controllers/genderController')
+const checkRole = require("../middleware/checkRoleMiddleware");
 
-router.post('/', genderController.create)
-router.delete('/', genderController.delete)
+router.post('/', checkRole('ADMIN'), genderController.create)
+router.delete('/', checkRole('ADMIN'), genderController.delete)
 router.get('/', genderController.getAll)
 
 module.exports = router
